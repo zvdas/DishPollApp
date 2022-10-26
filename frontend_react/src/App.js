dcd5b645a6c2cc0fee172728fe47eb0d6856452a
zvdas <rodriguezzude@gmail.com>
2022-10-26 10:43:11 +0530

    Bootstrap installed, configured & tested successfully

--- frontend_react/src/App.js
+++ frontend_react/src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container m-5 p-5'>
+      <h1 className='text-center'>Dish Poll App</h1>
     </div>
   );
 }
